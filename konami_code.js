9cec93bf29bc55ddf38592663daf225a901f7d29
Angela Perez <angela.perez@flatironschool.com>
2018-08-22 00:21:31 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -7,9 +7,10 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   //tracker keeps track of where we are in the array
   const body = document.querySelector('body');
+  const tracker = 0;
   body.addEventListener('keydown', (event) => {
     if (event.which === ) {
-      
+      tracker += 
     } 
   })
 }
